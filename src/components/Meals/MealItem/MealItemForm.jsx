275666ff8b74ca1bd54e5bd699e686d9bdf24c34
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-10-16 11:09:38 +0530

    added ATC form on meals list

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
 const MealItemForm = () => {
-  return <form className={classes.form}>MealItemForm</form>;
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          type: "number",
+          id: "amount",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button type="submit">+ Add</button>
+    </form>
+  );
 };
 
 export default MealItemForm;
