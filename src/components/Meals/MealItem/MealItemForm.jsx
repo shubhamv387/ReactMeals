b8b7131d1a84c5597a0bb155d82304731d024203
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-10-16 10:45:48 +0530

    added dummy meals

---
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = () => {
+  return <form className={classes.form}>MealItemForm</form>;
+};
+
+export default MealItemForm;
