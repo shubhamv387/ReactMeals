580a579e200fe5e92c3f56cfb670e7324287dffa
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-10-16 10:23:02 +0530

    added meals section

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MealsSummary from "./MealsSummary";
+import AvailableMeaks from "./AvailableMeaks";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeaks />
+    </>
+  );
+};
+
+export default Meals;
